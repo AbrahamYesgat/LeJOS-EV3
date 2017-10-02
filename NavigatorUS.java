294dfd0269089080a2e43583919dbe1033c86d5f
Ali Shobeiri <alishobeiri@ymail.com>
2017-10-01 23:03:27 -0400

    Cookies

--- NavigatorUS.java
+++ NavigatorUS.java
@@ -18,9 +18,11 @@ public class NavigatorUS extends Thread implements UltrasonicController {
 	private int index = 0;
 	private int[] avgVal = new int[3];
 	
-	// variables to store sensor data
 	private int distance;
+	private double avgDistance = 20;
 	
+	private int count = 0;
+	private double[][] points = {{0, 2}, {1, 1}, {2, 2}, {2, 1}, {1, 0}};
 	
 	// wall follower variables
 	private static final int motorLow = 50, motorHigh = 200, bandCenter = 10, bandwidth = 3, FILTER_OUT = 20;
@@ -40,16 +42,108 @@ public class NavigatorUS extends Thread implements UltrasonicController {
 		this.TRACK = Lab3.TRACK;
 	}
 	
-	/**
-	 * Reads our sensor distance
-	 */
-	public int readUSDistance() {
-		return this.distance;
+	public void run() {
+		leftMotor.stop();
+		rightMotor.stop();
+			
+		for(; count < 5; count++) {
+			travelTo(points[count][0], points[count][1]);
+		}
 	}
 	
-	/**
-	 * Processes our sensor data and acts accordingly based on its readings
-	 */
+	private void travelTo(double x, double y) {
+		System.out.println("x " + x);
+		System.out.println("y " + y);
+		
+		navigating = true;
+		x= x*30.48;
+		y= y*30.48;
+		
+		
+		double deltaX = x - odometer.getX();
+		double deltaY = y - odometer.getY();
+		
+		double minAngle = Math.toDegrees(Math.atan2(deltaX, deltaY)) - odometer.getThetaDegrees();
+		
+		if (minAngle < -180) {
+			// System.out.println("minAngle < -180");
+			minAngle = 360 + minAngle;
+			// System.out.println("minAngle: " + minAngle);
+		} else if (minAngle > 180) {
+			minAngle = minAngle - 360;
+		}
+		// turn to the minimum angle
+		turnTo(minAngle);
+		
+		// calculate the distance to next point
+		double distance  = Math.hypot(deltaX, deltaY);
+		
+		// move to the next point
+		leftMotor.setSpeed(FORWARD_SPEED);
+		rightMotor.setSpeed(FORWARD_SPEED);
+		leftMotor.rotate(convertDistance(RADIUS,distance), true);
+		rightMotor.rotate(convertDistance(RADIUS, distance), true);
+
+		while((leftMotor.isMoving() && rightMotor.isMoving())) {
+			if (followingWall) {
+				if (initialTheta - odometer.getThetaDegrees() >= 50) {
+		    		System.out.println("Theta statement");
+
+		    		leftMotor.stop(true);
+		    		rightMotor.stop(true);
+		    		sensorMotor.rotate(-60);
+		    		followingWall = false;
+		    		count --;
+		    		break;
+		    	} else {
+		    		System.out.println("Bang Bang Bang");
+				    if (avgDistance <= 10) {
+				    	System.out.println("less than 15");
+				    	leftMotor.setSpeed(motorHigh); // Start robot moving forward
+				        rightMotor.setSpeed(motorHigh);
+				        leftMotor.forward();
+				        rightMotor.backward();
+				    } else if (avgDistance > 20 && avgDistance < 30) {
+				    	System.out.println("between 25 and 35");
+				        leftMotor.setSpeed(motorHigh); // Start robot moving forward
+				        rightMotor.setSpeed(motorHigh);
+				        leftMotor.forward();
+				        rightMotor.forward();
+				  	} else if (avgDistance >= 20) {
+				  		System.out.println("greater than 15");
+				        leftMotor.setSpeed(100); // Left turn
+				        rightMotor.setSpeed(motorHigh-20);//motorLow-20
+				        leftMotor.forward();
+				        rightMotor.forward();
+				    } else if (avgDistance <= 30){
+				    	System.out.println("less than 35");
+				        leftMotor.setSpeed(motorHigh); // Right turn
+				        rightMotor.setSpeed(100);
+				        leftMotor.forward();
+				        rightMotor.forward();
+				    }
+		    	}
+			}
+			
+			if(avgDistance <= 15 && !followingWall) {
+				System.out.println("Obstacle detected");
+				followingWall = true;
+				initialTheta = odometer.getThetaDegrees();
+				turnTo(90);
+				sensorMotor.rotate(60);
+			}
+		}
+			
+		
+	    leftMotor.stop(true);
+		rightMotor.stop(true);
+
+		navigating = false;
+		leftMotor.setSpeed(0);
+		rightMotor.setSpeed(0);
+	}
+	
+	
 	
 	@Override
 	public void processUSData(int distance) {
@@ -66,229 +160,52 @@ public class NavigatorUS extends Thread implements UltrasonicController {
 	    for (int i=0; i<avgVal.length; i++) {
 	    	avgDistance+=avgVal[i];
 	    }
-	    avgDistance = avgDistance / 3;
+	    avgDistance = avgDistance / 3; 
 	    
-	    this.distance = avgDistance;
-	    
-	    
-	    if(followingWall) {	
-	    	System.out.println("initialTheta: " + initialTheta);
-	    	System.out.println("getTheta: " + odometer.getTheta());
-	    	System.out.println("Subtract initial from current " + (initialTheta - Math.toDegrees(odometer.getTheta())));
-	    	if (initialTheta - Math.toDegrees(odometer.getTheta()) >= 90) {
-	    		sensorMotor.rotate(-45);
-	    		turnTo(Math.toRadians(90.0));
-	    		leftMotor.stop(true);
-	    		rightMotor.stop(true);
-	    	} else {
-			    if (avgDistance <= 15) {
-			    	leftMotor.setSpeed(motorHigh); // Start robot moving forward
-			        rightMotor.setSpeed(motorHigh);
-			        leftMotor.forward();
-			        rightMotor.backward();
-			    } else if (avgDistance > 25 && distance < 35) {
-			        leftMotor.setSpeed(motorHigh); // Start robot moving forward
-			        rightMotor.setSpeed(motorHigh);
-			        leftMotor.forward();
-			        rightMotor.forward();
-			  	} else if (avgDistance >= 25) {
-			        leftMotor.setSpeed(100); // Left turn
-			        rightMotor.setSpeed(motorHigh-20);//motorLow-20
-			        leftMotor.forward();
-			        rightMotor.forward();
-			    } else if (avgDistance <= 35){
-			        leftMotor.setSpeed(motorHigh); // Right turn
-			        rightMotor.setSpeed(100);
-			        leftMotor.forward();
-			        rightMotor.forward();
-			    }
-	    	}
-	    } else if (avgDistance <= 15) {
-	        leftMotor.stop(true); 
-	        rightMotor.stop(true);
-	        initialTheta = odometer.getThetaDegrees();
-	        turnTo(Math.toRadians(90.0));
-	        sensorMotor.rotate(45);
-	        followingWall = true;
-	        System.out.println(initialTheta);
+	    if (followingWall) {
+	    	this.avgDistance = avgDistance / Math.sqrt(2);
 	    } else {
-	    	return;
+	    	this.avgDistance = avgDistance;
 	    }
-	    
-//	    else if (avgDistance > 25 && distance < 35) {
-//	        leftMotor.setSpeed(motorHigh); // Start robot moving forward
-//	        rightMotor.setSpeed(motorHigh);
-//	        leftMotor.forward();
-//	        rightMotor.forward();
-//	  	} else if (avgDistance >= 25) {
-//	        leftMotor.setSpeed(100); // Left turn
-//	        rightMotor.setSpeed(motorHigh-20);//motorLow-20
-//	        leftMotor.forward();
-//	        rightMotor.forward();
-//	    } else if (avgDistance <= 35){
-//	        leftMotor.setSpeed(motorHigh); // Right turn
-//	        rightMotor.setSpeed(100);
-//	        leftMotor.forward();
-//	        rightMotor.forward();
-//	    }
 	    
 	    this.distance = avgDistance;
 	  }
 
-	
-	/**
-	 * Our main run method
-	 * 
-	 */
-	public void run() {
-		leftMotor.stop();
-		leftMotor.setAcceleration(MOTOR_ACCELERATION);
-		rightMotor.stop();
-		rightMotor.setAcceleration(MOTOR_ACCELERATION);
 
-		travelTo(0, 1);
-		travelTo(1, 2);
-		travelTo(1, 0);
-		travelTo(2, 1);
-		travelTo(2, 2);
-	}
 	
-	/**
-	 * Determine how much the motor must rotate for vehicle to reach a certain distance
-	 * 
-	 * @param radius
-	 * @param distance
-	 * @return
-	 */
 	private static int convertDistance(double radius, double distance) {
 		return (int) ((180.0 * distance) / (Math.PI * radius));
 	}
 
-	/**
-	 * Determine the angle our motors need to rotate in order for vehicle to turn a certain angle 
-	 * 
-	 * @param radius
-	 * @param TRACK
-	 * @param angle
-	 * @return
-	 */
+
 	private static int convertAngle(double radius, double TRACK, double angle) {
 		return convertDistance(radius, Math.PI * TRACK * angle / 360.0);
 	}
 	
-	/**
-	 * A method to drive our vehicle to a certain cartesian coordinate
-	 * 
-	 * @param x X-Coordinate
-	 * @param y Y-Coordinate
-	 */
-	private void travelTo(double x, double y) {
-		
-//		System.out.println(" ");
-//		System.out.println("Travelling to x: " + x + ", y: " + y);
-		
-		navigating = true;
-		x= x*30.48;
-		y= y*30.48;
-		
-		System.out.println("x " + x);
-		System.out.println("y " + y);
-		
-//		System.out.println("Odometer X " + odometer.getX());
-//		System.out.println("Odometer Y " + odometer.getY());
-		
-		double deltaX = x - odometer.getX();
-		double deltaY = y - odometer.getY();
-		
-//		System.out.println("deltaX: " + deltaX);
-//		System.out.println("deltaY: " + deltaY);
-//
-//		System.out.println(" ");
-		
-		// calculate the minimum angle
-		double minAngle = Math.toDegrees(Math.atan2(deltaX, deltaY)) - odometer.getThetaDegrees();
-		
-		System.out.println("minAngle before correction " + minAngle);
-		
-		if (minAngle < -180) {
-			// System.out.println("minAngle < -180");
-			minAngle = 360 + minAngle;
-			// System.out.println("minAngle: " + minAngle);
-		} else if (minAngle > 180) {
-			System.out.println("minAngle > 180");
-			minAngle = minAngle - 360;
-			System.out.println("minAngle: " + minAngle);
-		}
-		// turn to the minimum angle
-		turnTo(minAngle);
-		
-		// calculate the distance to next point
-		double distance  = Math.hypot(deltaX, deltaY);
-		
-		// move to the next point
-		leftMotor.setSpeed(FORWARD_SPEED);
-		rightMotor.setSpeed(FORWARD_SPEED);
-		leftMotor.rotate(convertDistance(RADIUS,distance), true);
-		rightMotor.rotate(convertDistance(RADIUS, distance), false);
-//pause
-		leftMotor.stop(true);
-		rightMotor.stop(true);
-		//correction?
-		//odometer.setX(x + odometer.getX());
-		//odometer.setY(y + odometer.getY());
-		//odometer.setTheta(Math.atan2( deltaX, deltaY ) - odometer.getTheta());
-		navigating = false;
-		leftMotor.setSpeed(0);
-		rightMotor.setSpeed(0);
-	}
-	
-	/**
-	 * A method to turn our vehicle to a certain angle
-	 * 
-	 * @param theta
-	 */
+
 	private void turnTo(double theta) {
+		
 		leftMotor.setSpeed(ROTATE_SPEED);
 		rightMotor.setSpeed(ROTATE_SPEED);
 		
 		if(theta < 0) { // if angle is negative, turn to the left
-			leftMotor.rotate(-convertAngle(RADIUS, TRACK, -(theta*180)/Math.PI), true);
-			rightMotor.rotate(convertAngle(RADIUS, TRACK, -(theta*180)/Math.PI), false);
+			leftMotor.rotate(-convertAngle(RADIUS, TRACK, -theta), true);
+			rightMotor.rotate(convertAngle(RADIUS, TRACK, -theta), false);
 		} 
 		else { // angle is positive, turn to the right
-			leftMotor.rotate(convertAngle(RADIUS, TRACK, (theta*180)/Math.PI), true);
-			rightMotor.rotate(-convertAngle(RADIUS, TRACK, (theta*180)/Math.PI), false);
+			leftMotor.rotate(convertAngle(RADIUS, TRACK, theta), true);
+			rightMotor.rotate(-convertAngle(RADIUS, TRACK, theta), false);
 		}
 		
+//		leftMotor.setSpeed(0);
+//		rightMotor.setSpeed(0);
 	}
 
-	
-	/**
-	 * A method to that implements our wall following logic
-	 */
-//	private void excecuteWallFollow() {
-//		// calculate our offset from the bandCenter
-//		int error = distance - bandCenter - 5; // -5 for distance from sensor to side of vehicle
-//		
-//		// Keep moving forward if vehicle is within threshold value
-//		if ( Math.abs(error) < this.bandwidth ) {
-//			steerStraight();
-//		} 
-//		// We are too close to the wall, steer vehicle to the right
-//		else if ( error < 0 ) {
-//			steerRight(); 
-//		} 
-//		// We are too far away from the wall
-//		else { 
-//			if ( error > 100 ) {
-//				// It is just sensing something very far away, keep going straight
-//				steerStraight(); 
-//			} else {
-//				// We are too far from the wall, steer left
-//				steerLeft();
-//			}
-//		}
-//	}
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
+
 	
 	
 
